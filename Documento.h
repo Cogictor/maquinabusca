0ff6870f23447de24eb866890394a5836d31433b
Cogictor <vicdaniel.ferreira@gmail.com>
2019-11-01 22:22:38 -0300

    subindo arquivos Documeto.cpp Documento.h indice_invertido.cpp

---
+++ Documento.h
@@ -0,0 +1,12 @@
+#ifndef DOCUMENTO_H_
+#define DOCUMENTO_H_
+
+class Documento{
+    
+}
+
+
+
+
+
+#endif 
---
+++ indice_invertido.cpp
@@ -0,0 +1,5 @@
+//criar funcao que abre 
+
+// documento é um vector para armazenar a informacao e facilitar a busca e acessso aos elementos nos indices (Nao é estritamente necessario cresce-lo ao cria-lo)
+// documento é uma lista encadeada caso seja preciso cresce-lo
+
